a2c823f847177a8c6b12723fe257b502f12e946b
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2023-11-25 09:15:12 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+camera.position.z = 5;
+
+const geometry2 = new THREE.BoxGeometry(1, 1, 1);
+const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube2 = new THREE.Mesh(geometry2, material2);
+cube2.position.x = 2;
+
+const geometry3 = new THREE.BoxGeometry(1, 1, 1);
+const material3 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const cube3 = new THREE.Mesh(geometry3, material3);
+cube3.position.x = -2;
+
+scene.add(cube);
+scene.add(cube2);
+scene.add(cube3);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  cube2.rotation.x += 0.01;
+  cube2.rotation.y += 0.01;
+
+  cube3.rotation.x += 0.01;
+  cube3.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+console.log("check to connected mainjs");
